f211444bb7c72e55077631ca2b328ec462f11ca4
gurubac <gbrbachchu@gmail.com>
2021-11-03 23:56:13 -0700

    auth with server on PORT 3000, delete react proxy code

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -8,6 +8,12 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <button>
+            <a href="/login">Login</a>
+          </button>
+          <button>
+            <a href="/logout">Logout</a>
+          </button>
         </p>
         <a
           className="App-link"
--- server/src/server.js
+++ server/src/server.js
@@ -7,38 +7,36 @@ import Database from "./classes/Database.js";
 dotenv.config({ path: "../config/.env" });
 const port = process.env.PORT;
 const dbConn = process.env.DB_CONN;
+const secret = process.env.SECRET;
+const clientID = process.env.CLIENT_ID;
 
 const app = express();
 
 const database = new Database(dbConn);
 
-async function main() {
-  // await database.createUser({
-  //   email: "anthonyjbenjamin@gmail.com",
-  //   username: "copbrick",
-  //   password: "ballz",
-  // });
+//auth stuff
+import { auth } from "express-openid-connect" 
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: `${secret}`,
+  baseURL: 'http://localhost:3000',
+  clientID: `${clientID}`,
+  issuerBaseURL: 'https://dev--qv8kyr1.us.auth0.com'
+};
 
-  // await database.findUser("anthonyjbenjamin@gmail.com");
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
 
-  // await database.removeUser("anthonyjbenjamin@gmail.com");
+app.get('/', (req, res) => {
+  res.sendFile('index.html', { root: '../' })
+  // res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+  console.log(req.oidc.user);
+});
+//auth stuff
 
-  // await database.updateStatistics("anthonyjbenjamin@gmail.com", {
-  //   average: "1900",
-  //   averageof5: "2000",
-  // });
-
-  // await database.updateSettings("anthonyjbenjamin@gmail.com", {
-  //   newUIMode: "dark",
-  // });
-
-  // await database.clearStatistics("anthonyjbenjamin@gmail.com");
-}
-
-main().then(() => signale.success("Finished Main Function!"));
-
-import home from "./routes/home.js";
-app.use("/", home);
+// import home from "./routes/home.js";
+// app.use("/", home);
 
 app.listen(port, () => {
   signale.success(`Server is running on http://localhost:${port}`);
