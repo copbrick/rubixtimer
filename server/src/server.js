2732567b61f8697e2b57de9771b31339cb7a205b
gurubac <gbrbachchu@gmail.com>
2021-11-04 12:24:27 -0700

    update

--- server/src/server.js
+++ server/src/server.js
@@ -3,6 +3,8 @@ import express from "express";
 import signale from "signale";
 import signaleConfig from "../config/signaleConfig.js";
 import Database from "./classes/Database.js";
+import path from "path";
+const __dirname = path.resolve();
 
 dotenv.config({ path: "../config/.env" });
 const port = process.env.PORT;
@@ -20,20 +22,35 @@ const config = {
   authRequired: false,
   auth0Logout: true,
   secret: `${secret}`,
-  baseURL: 'http://localhost:3000',
+  baseURL: 'http://localhost:4000',
   clientID: `${clientID}`,
   issuerBaseURL: 'https://dev--qv8kyr1.us.auth0.com'
 };
 
 // auth router attaches /login, /logout, and /callback routes to the baseURL
+
+app.use(express.static(path.resolve(__dirname, '../../client/build')));
 app.use(auth(config));
 
+// // All other GET requests not handled before will return our React app
 app.get('/', (req, res) => {
-  res.sendFile('index.html', { root: '../' })
-  // res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-  console.log(req.oidc.user);
+  res.sendFile(path.resolve(__dirname, '../../client/build', 'index.html'));
+  console.log(path.resolve(__dirname, '../../client/build', 'index.html'))
+  console.log(req.oidc.isAuthenticated());
 });
-//auth stuff
+
+// app.get('/:error', (req, res) => {
+//   res.send(404, 'Not found')
+// });
+
+
+//test index html route in server dir
+
+// app.get('/', (req, res) => {
+//   res.sendFile('index.html', { root: '../' })
+//   console.log(req.oidc.user);
+// });
+
 
 // import home from "./routes/home.js";
 // app.use("/", home);
