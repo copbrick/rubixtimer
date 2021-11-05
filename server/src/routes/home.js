cbd57899d23ff7b310931aa389aa588a217cb2d1
anthony <anthonyjbenjamin@gmail.com>
2021-11-05 15:57:30 -0700

    n

--- server/src/routes/home.js
+++ server/src/routes/home.js
@@ -6,9 +6,11 @@ const __dirname = path.resolve();
 import signale from "signale";
 import signaleConfig from "../../config/signaleConfig.js";
 
+
+//get access to user object by doing req.oidc.user
 router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../../client/build", "index.html"));
-  console.log("Root Request Made: " + req.oidc.user);
+  console.log(req.oidc.user);
 });
 
 export default router;
