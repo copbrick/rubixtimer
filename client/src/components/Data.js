4ac40a86a9732af5cab4f3e3e0252f633ec6b20a
anthony <anthonyjbenjamin@gmail.com>
2021-11-10 23:51:10 -0800

    finish render data

--- client/src/components/Data.js
+++ client/src/components/Data.js
@@ -2,31 +2,30 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function Data() {
+  //fetch data from api with axios
   const [data, setData] = useState([]);
-  useEffect(async () => {
-    const response = await axios.get(
-      //   "https://jsonplaceholder.typicode.com/users"
-      "http://localhost:4000/api/user"
-    );
-    setData(response.data);
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }, []);
+
   return (
     <div>
       <h1>Data</h1>
       <ul>
-        {/* {data.map((item) => (
-          <li key={item.id}>{item.name}</li>
-        ))} */}
-        {console.log("FE" + data)}
-
-        {/* {data.map((item) => (
-          <li key={item.user.email}></li>
-        ))} */}
-
-        <li> {data}</li>
+        {data.map((item) => (
+          <li key={item.id}>
+            {item.name} - {item.email}
+          </li>
+        ))}
       </ul>
     </div>
   );
 }
-
 export default Data;
