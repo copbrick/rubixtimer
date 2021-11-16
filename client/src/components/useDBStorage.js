61e52576cbd750ae400a603df63ea93f88c2959b
gurubac <gbrbachchu@gmail.com>
2021-11-15 22:04:54 -0800

    complete post background function

--- client/src/components/useDBStorage.js
+++ client/src/components/useDBStorage.js
@@ -2,38 +2,44 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import { baseURL } from "../config/BaseURL";
 
-function getStorageValue() {
+function getStorageValue(setValue) {
   //get from mongoDB
   axios
     .get(`${baseURL}/api/user/`)
     .then((res) => {
       console.log("res data from get " + res.data.settings[0].backgroundColor);
-      return res.data.settings[0].backgroundColor;
+      if (res.data.settings[0].backgroundColor === null) {
+        console.log("its null");
+      } else {
+        setValue(res.data.settings[0].backgroundColor);
+      }
     })
     .catch((err) => {
-      return ("err" + err);
+      return "err" + err;
     });
 }
 
 export const useDBStorage = (key, defaultValue) => {
-  const [value, setValue] = useState(() => {
-    console.log("line 20 get storage val " + getStorageValue());
-    return getStorageValue();
-  });
+  const [value, setValue] = useState(defaultValue);
 
-  const url = `${baseURL}/api/update/settings`;
-  const body = { backgroundColor: value };
   useEffect(() => {
+    getStorageValue(setValue);
+  }, []);
+
+  const set = (newValue) => {
+    setValue(newValue);
+    const url = `${baseURL}/api/update/settings`;
+    const body = { backgroundColor: newValue };
     axios
-      .post(url,  body)
+      .post(url, body)
       .then((res) => {
-        setValue(res.data.backgroundColor);
         console.log("post res" + res);
       })
       .catch((err) => {
         console.log("post err" + err);
       });
-  }, [key, value]);
+  };
+  useEffect(() => {}, [key, value]);
 
-  return [value, setValue];
+  return [value, set];
 };
