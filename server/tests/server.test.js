99a31bf5d749ea0da15c9402e84bfd3890fcb3cd
gurubac <gbrbachchu@gmail.com>
2021-11-20 21:21:43 -0800

    test

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,6 +1,14 @@
+import {jest} from '@jest/globals';
 import { app } from "../src/server.js";
 import supertest from "supertest";
 
-test("GET /api/user", async () => {
-  await supertest(app).get("/api/user").expect(302);
+beforeAll(() => {
+  jest.useFakeTimers();
 });
+
+describe("api test", () => {
+  it("should GET /api/user", async () => {
+    await supertest(app).get("/api/user").expect(302)
+  });
+});
+
