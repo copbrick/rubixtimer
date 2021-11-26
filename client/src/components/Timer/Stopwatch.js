a1ce447f9d0374af82c2f35dd7e1d9ace4fd1d0c
anthony <anthonyjbenjamin@gmail.com>
2021-11-26 00:57:27 -0800

    latest code, fix post API made from client-side timer

--- client/src/components/Timer/Stopwatch.js
+++ client/src/components/Timer/Stopwatch.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 // import "./Stopwatch.css";
 import DisplayTimer from "./DisplayTimer";
 import ControlTimer from "./ControlTimer";
